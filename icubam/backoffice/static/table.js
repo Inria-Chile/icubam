d86876cc0ff7e2c7d523e9ed0265d92a43dc50ce
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-04-13 13:02:28 -0400

    Add backoffice lists internationalization

--- icubam/backoffice/static/table.js
+++ icubam/backoffice/static/table.js
@@ -1,6 +1,9 @@
-function setDatatable (table_id) {
+function setDatatable (table_id, language_url) {
   let table = $(table_id).DataTable({
     "responsive": true,
     "autoWidth": false,
+    "language": {
+      "url": language_url,
+    },
   })
 }
